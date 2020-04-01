dd677c984dfbbbdb0f5b20b4c1f2dabf2e1ec019
Wawesh2020 <ghenry429@gmail.com>
2020-04-01 12:48:54 -0400

    Made changes to Calculate function so all tests run

--- index.js
+++ index.js
@@ -2,38 +2,65 @@
 
 
 function calculate(steps) {
-  let total = 0
+  let valueOne
   let operator
+  let valueTwo
 
-  let valueTwo = 0
 
 
   for (let i = 0; i < steps.length; i++) {
     if (!isNaN(parseInt(steps[i])) && typeof (steps[i]) === 'string') {
-      total = parseInt(steps[i])
-    }
-    if (typeof (steps[i]) === 'number') {
-      valueTwo = steps[i]
+      if (valueOne === undefined) {
+        valueOne = parseInt(steps[i])
+      } else {
+        valueTwo = parseInt(steps[i])
+      }
     }
 
+    if (typeof (steps[i]) === 'number') {
+      if (valueOne === undefined) {
+        valueOne = steps[i]
+      } else {
+        valueTwo = steps[i]
+      }
+    }
+
+
+
+    // checks if steps[i] is an operator
     if (steps[i] === '+' || steps[i] === '-' || steps[i] === '*' || steps[i] === '/') {
       operator = steps[i]
     }
 
-    //   if (typeof (steps[i]) === 'number' && !isNaN(parseInt(steps[i])) ) {
-    //     total = parseInt(steps[i])
-    //   }
-    //   if (typeof (steps[i]) < 1) { 
-    //     valueTwo = steps[i]
-    //   }
-    // }
+
+
+    if ((steps[i]) === null) {
+      if (valueOne === undefined) {
+        valueOne = 0
+      } else {
+        valueTwo = 0
+      }
+    }
   }
   if (operator === undefined) {
     return NaN
   }
   if (operator === '*') {
-    return total = total * valueTwo
+    return valueOne * valueTwo
+  }
+  if (operator === '+') {
+    return valueOne + valueTwo
+  }
+  if (operator === '-')
+  {
+    return valueOne - valueTwo
+  }
+  if (operator === '/')
+  {
+    return valueOne / valueTwo
   }
 }
+
+
 
 module.exports = calculate
